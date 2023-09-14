592261ebde74da622cbfb34bf5ff9712cac26cf5
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-14 11:10:24 -0700

    idk

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays
+{
+    public static void main(String[] args) {
+        String[] strArray={"Alpha","Bravo","Charlie","Delta","Echo"};
+        for (int i=0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+        
+        for (int i=0; i < strArray.length; i++) {
+            System.out.print(strArray[i]);
+        }
+        System.out.println();
+        for (int i=0; i < strArray.length; i++) {
+            System.out.print(strArray[i]);
+            if ( {
+                System.out.print("-");
+            }
+        }
+    }
+}
